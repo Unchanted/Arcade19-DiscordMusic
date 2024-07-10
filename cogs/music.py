7f5b99195a497de30ee66ba71aace0f2a0a576ae
Unchanted <omkkdwivedi@gmail.com>
2024-07-10 22:11:23 +0530

    music commands

--- cogs/music.py
+++ cogs/music.py
@@ -1,17 +1,95 @@
+from typing import cast
+
 import discord
 import wavelink
 
 from discord.ext import commands
 
 class Music(commands.Cog):
-    
-    @commands.command()
-    async def play(self, ctx: commands.Context):
-        await ctx.send(wavelink.nodes)
+    def __init__(self, bot) -> None:
+        self.bot = bot
 
     @commands.Cog.listener()
     async def on_wavelink_node_ready(self, payload: wavelink.NodeReadyEventPayload) -> None:
         print(f"Node {payload.node!r} is ready!")
+    
+    @commands.command()
+    async def play(self, ctx: commands.Context, *, query):
+        """Play a song with the given query."""
+        if not ctx.guild:
+            return
+
+        player: wavelink.Player
+        player = cast(wavelink.Player, ctx.voice_client)  # type: ignore
+
+        if not player:
+            try:
+                player = await ctx.author.voice.channel.connect(cls=wavelink.Player)  # type: ignore
+            except AttributeError:
+                await ctx.send("Please join a voice channel first before using this command.")
+                return
+            except discord.ClientException:
+                await ctx.send("I was unable to join this voice channel. Please try again.")
+                return
+
+        player.autoplay = wavelink.AutoPlayMode.enabled
+
+        if not hasattr(player, "home"):
+            player.home = ctx.channel
+        elif player.home != ctx.channel:
+            await ctx.send(f"You can only play songs in {player.home.mention}, as the player has already started there.")
+            return
+
+        tracks: wavelink.Search = await wavelink.Playable.search(query)
+        if not tracks:
+            await ctx.send(f"{ctx.author.mention} - Could not find any tracks with that query. Please try again.")
+            return
+
+        if isinstance(tracks, wavelink.Playlist):
+            added: int = await player.queue.put_wait(tracks)
+            await ctx.send(f"Added the playlist **`{tracks.name}`** ({added} songs) to the queue.")
+        else:
+            track: wavelink.Playable = tracks[0]
+            await player.queue.put_wait(track)
+            await ctx.send(f"Added **`{track}`** to the queue.")
+
+        if not player.playing:
+            await player.play(player.queue.get(), volume=30)
+
+        try:
+            await ctx.message.delete()
+        except discord.HTTPException:
+            pass
+
+    @commands.command()
+    async def skip(self, ctx: commands.Context) -> None:
+        """Skip the current song."""
+        player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+        if not player:
+            return
+
+        await player.skip(force=True)
+        await ctx.message.add_reaction("\u2705")
+
+    @commands.command(name="toggle", aliases=["pause", "resume"])
+    async def pause_resume(self, ctx: commands.Context) -> None:
+        """Pause or Resume the Player depending on its current state."""
+        player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+        if not player:
+            return
+
+        await player.pause(not player.paused)
+        await ctx.message.add_reaction("\u2705")
+
+    @commands.command(aliases=["dc"])
+    async def disconnect(self, ctx: commands.Context) -> None:
+        """Disconnect the Player."""
+        player: wavelink.Player = cast(wavelink.Player, ctx.voice_client)
+        if not player:
+            return
+
+        await player.disconnect()
+        await ctx.message.add_reaction("\u2705")
 
 async def setup(bot: commands.Bot):
-    await bot.add_cog(Music())
+    await bot.add_cog(Music(bot))
